fdad273154f838cc273b87ae87af25bd5feadafe
taeden211 <162815730+taeden211@users.noreply.github.com>
2024-06-19 01:33:48 +0000

    기말고사 끝

--- app.js
+++ app.js
@@ -97,19 +97,14 @@ router.use((req, res, next) => {
  * =====================================================================
  */
 
-// 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "ut-nodejs";
-
+const mongoose = require("mongoose"); // mongoose를 요청
 // 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
-});
-
-// 연결되면 메시지를 보냄
+mongoose.connect(
+  "mongodb+srv://ut-node:ZCLz72q3RJMiD8GW@taeden.wxgncva.mongodb.net/?retryWrites=true&w=majority&appName=taeden,",
+);
 const db = mongoose.connection;
 db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+  console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
 /**
@@ -184,6 +179,28 @@ router.delete(
   usersController.redirectView
 );
 
+
+
+router.get("/users", usersController.index, usersController.indexView); // index 라우트 생성
+router.get("/users/new", usersController.new); // 생성 폼을 보기 위한 요청 처리
+router.post(
+  "/users/create",
+  usersController.create,
+  usersController.redirectView
+); // 생성 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
+router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit); // viewing을 처리하기 위한 라우트 추가
+router.put(
+  "/users/:id/update",
+  usersController.update,
+  usersController.redirectView
+); // 편집 폼에서 받아온 데이터의 처리와 결과를 사용자 보기 페이지에 보여주기
+router.delete(
+  "/users/:id/delete",
+  usersController.delete,
+  usersController.redirectView
+);
+
 /**
  * =====================================================================
  * @TODO:
@@ -195,12 +212,31 @@ router.delete(
  * =====================================================================
  */
 // 1. index 라우트 생성 (모든 레코드 보기) = GET /discussions,                index 액션, index 뷰
-// 2. 생성 폼을 보기 위한 요청 처리        = GET /discussions/new,            new 액션
+router.get("/discussions", discussionsController.index, discussionsController.indexView);
+// 2. 생성 폼을 보기 위한 요청 처리        = GET /discussions/new,            new 액션 
+router.get("/discussions/new", discussionsController.new); // 생성 폼을 보기 위한 요청 처리
 // 3. 생성 데이터의 처리와 결과            = POST /discussions/create,        create 액션, redirectView 뷰
-// 4. show를 처리하기 위한 라우트          = GET /discussions/:id,            show 액션, showView 뷰
+router.post(
+  "/discussions/create",
+  discussionsController.create,
+  discussionsController.redirectView
+)
+// 4. show를 처리하기 위한 라우트          = GET /discussions/:id,            show 액션, showView 뷰; 
+router.get("/discussions/:id", discussionsController.show, discussionsController.showView);
 // 5. edit를 처리하기 위한 라우트          = GET /discussions/:id/edit,       edit 액션
+router.get("/discussions/:id/edit", discussionsController.edit); 
 // 6. 편집 데이터의 처리와 결과            = PUT /discussions/:id/update,     update 액션, redirectView 뷰
+router.put(
+  "/discussions/:id/update",
+  discussionsController.update,
+  discussionsController.redirectView
+);
 // 7. 삭제를 처리하기 위한 라우트          = DELETE /discussions/:id/delete,  delete 액션, redirectView 뷰
+router.delete(
+  "/discussions/:id/delete",
+  discussionsController.delete,
+  discussionsController.redirectView
+);
 
 /**
  * Comments
